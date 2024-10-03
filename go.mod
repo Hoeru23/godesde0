ee7b9f6e5f72a9208ea07630ca018d831cfb982d
Joel Matos <joel_matos@bhdleon.com.do>
2024-10-03 15:34:00 -0400

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Hoeru23/godesde0
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Hoeru23/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
